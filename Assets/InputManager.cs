7202b950aab1390c35b4f79145945424c903aae1
baligaandris <baligaandris@gmail.com>
2018-06-18 16:45:39 +0100

    Prototype works

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -22,38 +22,40 @@ public class InputManager : MonoBehaviour {
     {
         foreach (Touch touch in Input.touches)
         {
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            if (Input.touchCount > 0)
             {
-                clickLocation = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                clickLocation = new Vector3(clickLocation.x, clickLocation.y, 0);
-                line.enabled = true;
-                Debug.Log("click");
-                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
-                if (hit.collider != null)
+                if (Input.touches[touch.fingerId].phase == TouchPhase.Began)
                 {
-                    Debug.Log(hit.transform.gameObject.name);
-                    bodyPartClicked = hit.transform.gameObject;
+                    clickLocation = Camera.main.ScreenToWorldPoint(Input.touches[touch.fingerId].position);
+                    clickLocation = new Vector3(clickLocation.x, clickLocation.y, 0);
+                    line.enabled = true;
+                    Debug.Log("click");
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[touch.fingerId].position), Vector2.zero);
+                    if (hit.collider != null)
+                    {
+                        Debug.Log(hit.transform.gameObject.name);
+                        bodyPartClicked = hit.transform.gameObject;
+                    }
                 }
-            }
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                if (clickLocation != null)
+                else if (Input.touches[touch.fingerId].phase == TouchPhase.Moved)
                 {
-                    line.SetPosition(0, clickLocation);
-                    currentMousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y, 0);
-                    line.SetPosition(1, currentMousePos);
-                }
-            }
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
-            {
-                line.enabled = false;
-                if (bodyPartClicked != null)
-                {
-                    bodyPartClicked.GetComponent<Rigidbody2D>().AddForceAtPosition((clickLocation - currentMousePos) * forceMultiplier, clickLocation);
-                    bodyPartClicked = null;
+                    if (clickLocation != null)
+                    {
+                        line.SetPosition(0, clickLocation);
+                        currentMousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.touches[touch.fingerId].position).x, Camera.main.ScreenToWorldPoint(Input.touches[touch.fingerId].position).y, 0);
+                        line.SetPosition(1, currentMousePos);
+                    }
                 }
 
-
+                else if (Input.touches[touch.fingerId].phase == TouchPhase.Ended)
+                {
+                    line.enabled = false;
+                    if (bodyPartClicked != null)
+                    {
+                        bodyPartClicked.GetComponent<Rigidbody2D>().AddForceAtPosition((clickLocation - currentMousePos) * forceMultiplier, clickLocation);
+                        bodyPartClicked = null;
+                    }
+                }
             }
         }
        
---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount>0)
+        {
+            transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.touches[0].position).x, Camera.main.ScreenToWorldPoint(Input.touches[0].position).y,0);
+        }
+	}
+}
